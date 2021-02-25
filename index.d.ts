0f0ba6911e900ecf1d3c25dcfbf834b9e63a38cd
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-02-25 09:52:34 -0300

    add simple documentation

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,18 @@
+/**
+ * The path to chrome in the current machine.
+ * 
+ * `null` if the current OS is Solaris
+ */
 export const chrome: string | null;
+/**
+ * The path to chrome canary in the current machine.
+ * 
+ * `null` if the current OS is Solaris
+ */
 export const chromeCanary: string | null;
+/**
+ * The path to chromium in the current machine.
+ * 
+ * `null` if the current OS is Solaris
+ */
 export const chromium: string | null;
